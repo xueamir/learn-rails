0dd3f49123191b341478d88bf0ab7b15e529fef2
Dominique Monayong Jr <Dominique.Monayong@gmail.com>
2016-09-25 23:34:48 -0400

    front end

--- learn-rails/app/controllers/visitors_controller.rb
+++ learn-rails/app/controllers/visitors_controller.rb
@@ -2,8 +2,6 @@ class VisitorsController < ApplicationController
   def new
     Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    flash[:notice] = 'Welcome!'
-    flash[:alert] = 'My birthday is soon.'
     Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
   end
 end
