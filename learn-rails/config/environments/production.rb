d6556dd17831461e16ffbabc56e7f2c69949b124
Dominique Monayong Jr <Dominique.Monayong@gmail.com>
2016-10-01 20:53:09 -0400

    Mail for production added

--- learn-rails/config/environments/production.rb
+++ learn-rails/config/environments/production.rb
@@ -44,6 +44,23 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+# email enabled in production
+ config.action_mailer.smtp_settings = {
+   address: "smtp.sendgrid.net",
+   port: 587,
+   domain: Rails.application.secrets.domain_name,
+   authentication: "plain",
+   user_name: Rails.application.secrets.email_provider_username,
+   password: Rails.application.secrets.email_provider_password
+ }
+ # ActionMailer Config
+ config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+ config.action_mailer.delivery_method = :smtp
+ config.action_mailer.perform_deliveries = true
+ config.action_mailer.raise_delivery_errors = false
+
+
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
